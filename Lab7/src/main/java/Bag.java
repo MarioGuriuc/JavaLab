efe0274cb2cdd4bbc5044db20f55611a4a6d42a2
MarioGuriuc <mario.guriuc@student.uaic.ro>
2024-04-12 01:28:10 +0300

    [Lab 7] Compulsory + (Homework / 2)
    Signed-off-by: MarioGuriuc <mario.guriuc@student.uaic.ro>

--- Lab7/src/main/java/Bag.java
+++ Lab7/src/main/java/Bag.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Bag {
+import java.util.*;
+
+public class Bag
+{
+	private final List<Token> tokens = new ArrayList<>();
+	private final int TOKENS = 5;
+
+	public Bag()
+	{
+		for (int i = 0; i <= TOKENS; i++)
+		{
+			for (int j = 0; j <= TOKENS; j++)
+			{
+				if (i != j)
+				{
+					tokens.add(new Token(i, j));
+				}
+			}
+		}
+
+		Collections.shuffle(tokens);
+	}
+
+	public synchronized Token extractToken()
+	{
+		if (tokens.isEmpty())
+		{
+			return null;
+		}
+		return tokens.remove(0);
+	}
+
+	public List<Token> getTokens()
+	{
+		return tokens;
+	}
 }
--- Lab7/src/main/java/Player.java
+++ Lab7/src/main/java/Player.java
@@ -1,2 +1,74 @@
-package PACKAGE_NAME;public class Player {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements Runnable
+{
+	private final String name;
+	private final Game game;
+	private final List<Token> sequences;
+	private boolean running;
+
+	public Player(String name, Game game)
+	{
+		this.name = name;
+		this.game = game;
+		sequences = new ArrayList<>();
+		running = true;
+	}
+
+	@Override
+	public synchronized void run()
+	{
+		while (running)
+		{
+			this.pickToken();
+		}
+	}
+
+	public void pickToken()
+	{
+		synchronized (game)
+		{
+			if (game.getBag().getTokens().isEmpty())
+			{
+				running = false;
+				game.notifyAll();
+				return;
+			}
+
+			Bag bag = game.getBag();
+
+			while (game.getPlayersTurn() != this)
+			{
+				try
+				{
+					//System.out.println(this.name + " is waiting for their turn.");
+					game.wait();
+				}
+				catch (InterruptedException e)
+				{
+					System.exit(1);
+				}
+			}
+			//System.out.println(name + " is picking a token.");
+			Token token = bag.extractToken();
+			if (token != null)
+			{
+				sequences.add(token);
+				game.updatePlayersTurn();
+			}
+			//System.out.println(name + " has picked token: " + token);
+		}
+	}
+
+	public List<Token> getSequences()
+	{
+		return sequences;
+	}
+
+	@Override
+	public String toString()
+	{
+		return name;
+	}
 }
